76bdd9784e8dc41bb2660ef3aa218a9a67a6d125
Ashton B <ashtonbrugh@gmail.com>
2021-10-26 16:08:13 -0400

    adding an if statement for lengthChoice

--- Develop/script.js
+++ Develop/script.js
@@ -1,69 +1,47 @@
-var randomUserUpperCase = ["A", "B", "C", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-var randomUserLowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var randomUserNumberTrue = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-var randomUserSpecialCharacters = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+","-", ".", "/", ":", ";", "<", "=", ">", "?", "@","^", "`", "{", "|", "}", "~"];
+var randomUserUpperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var randomUserLowerCase = "abcdefghijklmnopqrstuvwxyz";
+var randomUserNumberTrue = "0123456789";
+var randomUserSpecialCharacters = "!#$%&'()*+-./:;<=>?@[\^_`{|}~";
 
-//validate each input, make sure at least one character type is selected
+
 
 //generate password
+function generatePassword() {
+  var passwordChoice = '';
+  var passwordWork = '';
 
-function generatePasswordLog (passwordLength, upperCase, lowerCase, numberTrue, specialCharacters, generatePassword) {
-  console.log(passwordLength);
-  console.log(upperCase);
-  console.log(lowerCase);
-  console.log(numberTrue);
-  console.log(specialCharacters);
-  console.log(generatePassword); 
-
-}
-
-function generatePassword () {
+  // validating password length is between 8-128 before moving on
+  var lengthChoice = prompt('Password length? Min 8 characters - Max 128 characters')
+      if (lengthChoice <= 8 || lengthChoice >= 128) {
+        alert("Password must be between 8 - 128 characters!");
+        return "";
+      }
+  var upperChoice  = confirm('Would you like upper case? Click OK for yes and Cancel for no')
+      
+  var lowerChoice = confirm('Would you like lower case? Click OK for yes and Cancel for no')
+      
+  var numberChoice = confirm('Would you like to include numbers? Click OK for yes and Cancel for no')
+     
+  var charactersChoice = confirm('Would you like to use special characters? Click OK for yes and Cancel for no')
+      
+      for (var i = 0; i < confirmLength; i++) {
+          randomPassword = randomPassword + passwordCharacters[Math.floor(Math.random() * passwordCharacters.length)];
+          console.log(randomPassword)
+        }
+        return randomPassword;
+  };
+       
   
-  var lengthChoice = window.prompt('Password length? Min 8 characters - Max 128 characters')
-  var upperChoice  = window.confirm('Would you like upper case? Click OK for yes and Cancel for no')
-  var lowerChoice = window.confirm('Would you like lower case? Click OK for yes and Cancel for no')
-  var numberChoice = window.confirm('Would you like to include numbers? Click OK for yes and Cancel for no')
-  var charactersChoice = window.confirm('Would you like to use special characters? Click OK for yes and Cancel for no')
 
-
-  if (lengthChoice >= 8 && lengthChoice <= 128){
-    
-  } else {
-    alert("pick a better number")
-    return "";
-  } 
-
-
-
-
-
-
-
-  return "test";
-
-  
-  
-  
-}
-
-
-
-//upperCase ()
-
-//lowerCase()
-
-//numberTrue()
-
-//specialCharacters()
 
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-function writePassword() {
+function writePassword(password) {
   
-  var password = generatePassword () 
+  var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
