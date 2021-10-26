5bfcdb715acea136e576c3510161ef3c909fb5b3
Ashton B <ashtonbrugh@gmail.com>
2021-10-26 14:45:01 -0400

    adding to generate password function

--- Develop/script.js
+++ Develop/script.js
@@ -1,52 +1,7 @@
-// Assignment code here
-//assigning arrays
-
-
-var randomUserUpperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var randomUserUpperCase = ["A", "B", "C", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 var randomUserLowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var randomUserNumberTrue = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var randomUserSpecialCharacters = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+","-", ".", "/", ":", ";", "<", "=", ">", "?", "@","^", "`", "{", "|", "}", "~"];
-
-//How long would you like your password to be? (min 8 - max 128)
-
-function passwordLength () {
-  var userPasswordLength =  window.prompt('Password length? Min 8 characters - Max 128 characters')
-  //console.log(userPasswordLength)
-  return userPasswordLength
-} 
-
-//Would you like upper case?
-
-function upperCase () {
-  var userUpperCase = window.confirm('Would you like upper case? Click OK for yes and Cancel for no')
-  //console.log(userUpperCase)
-  return userUpperCase
-}
-
-//Would you like lower case?
-
-function lowerCase () {
-  var userLowerCase = window.confirm('Would you like lower case? Click OK for yes and Cancel for no')
-  //console.log(userLowerCase)
-  return userLowerCase
-}
-
-
-//Would you like to include numbers?
-
-function numberTrue () {
-  var userNumberTrue = window.confirm('Would you like to include numbers? Click OK for yes and Cancel for no')
-  //console.log(userNumberTrue)
-  return userNumberTrue
-}
-
-//Would you like to include special characters?
-
-function specialCharacters () {
-  var userSpecialCharacters = window.confirm('Would you like to use special characters? Click OK for yes and Cancel for no')
- // console.log (userSpecialCharacters)
- return userSpecialCharacters
-}
 
 //validate each input, make sure at least one character type is selected
 
@@ -62,24 +17,34 @@ function generatePasswordLog (passwordLength, upperCase, lowerCase, numberTrue, 
 
 }
 
-const randomUserUpperCaseValue = Math.floor(Math.random() * randomUserUpperCase.length);
-//console.log(randomUserUpperCaseValue, randomUserUpperCase[random]);
-
 function generatePassword () {
-  if (upperCase == true & lowerCase == true & numberTrue == true & specialCharacters == true){
-    var gPassword = randomUserUpperCaseValue }
+  
+  var lengthChoice = window.prompt('Password length? Min 8 characters - Max 128 characters')
+  var upperChoice  = window.confirm('Would you like upper case? Click OK for yes and Cancel for no')
+  var lowerChoice = window.confirm('Would you like lower case? Click OK for yes and Cancel for no')
+  var numberChoice = window.confirm('Would you like to include numbers? Click OK for yes and Cancel for no')
+  var charactersChoice = window.confirm('Would you like to use special characters? Click OK for yes and Cancel for no')
 
-  else var gPassword = null
+
+  if (lengthChoice >= 8 && lengthChoice <= 128){
+    
+  } else {
+    alert("pick a better number")
+    return "";
+  } 
+
+
+
+
+
+
+
+  return "test";
+
   
   
-};
-
-
-
-// for passwordLength.length 
-// 
-
-
+  
+}
 
 
 
@@ -97,7 +62,8 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword (passwordLength(), upperCase(), lowerCase(), numberTrue(), specialCharacters());
+  
+  var password = generatePassword () 
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
@@ -106,3 +72,14 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+
+
+
+
+
+
+
+
+
