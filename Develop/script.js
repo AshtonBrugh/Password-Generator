41fca09aa9a71d6f7c87e1c768b413a42a78e247
Ashton B <ashtonbrugh@gmail.com>
2021-10-26 16:33:04 -0400

    adding if loops to the confirm prompts

--- Develop/script.js
+++ Develop/script.js
@@ -5,11 +5,10 @@ var randomUserSpecialCharacters = "!#$%&'()*+-./:;<=>?@[\^_`{|}~";
 
 
 
+
 //generate password
 function generatePassword() {
-  var passwordChoice = '';
-  var passwordWork = '';
-
+  var passwordCharacters = [];
   // validating password length is between 8-128 before moving on
   var lengthChoice = prompt('Password length? Min 8 characters - Max 128 characters')
       if (lengthChoice <= 8 || lengthChoice >= 128) {
@@ -17,14 +16,30 @@ function generatePassword() {
         return "";
       }
   var upperChoice  = confirm('Would you like upper case? Click OK for yes and Cancel for no')
+      if (upperChoice) {
+        passwordCharacters = passwordCharacters.concat(randomUserUpperCase)
+      }
       
   var lowerChoice = confirm('Would you like lower case? Click OK for yes and Cancel for no')
+     if (lowerChoice) {
+     passwordCharacters = passwordCharacters.concat(randomUserLowerCase)
+   }
       
   var numberChoice = confirm('Would you like to include numbers? Click OK for yes and Cancel for no')
+    if (numberChoice) {
+    passwordCharacters = passwordCharacters.concat(randomUserNumberTrue)
+  }
      
   var charactersChoice = confirm('Would you like to use special characters? Click OK for yes and Cancel for no')
+    if (charactersChoice) {
+    passwordCharacters = passwordCharacters.concat(randomUserSpecialCharacters);
+  }
+
+  console.log(passwordCharacters)
+
+    var randomPassword = "";
       
-      for (var i = 0; i < confirmLength; i++) {
+      for (var i = 0; i < lengthChoice; i++) {
           randomPassword = randomPassword + passwordCharacters[Math.floor(Math.random() * passwordCharacters.length)];
           console.log(randomPassword)
         }
@@ -48,8 +63,9 @@ function writePassword(password) {
 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+  // Add event listener to generate button
+  generateBtn.addEventListener("click", writePassword);
+
 
 
 
